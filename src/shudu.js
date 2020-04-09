544b34978e578f9405dbb06bbbc1d16354c71b19
JackKuo <kuohfjack@gmail.com>
2020-04-10 00:07:48 +0800

    Move conversion code to lib

--- src/shudu.js
+++ src/shudu.js
@@ -1,12 +1,27 @@
 'use strict'
 
-var args = process.argv;
-var origin = "";
-if (args.length < 3) {
-    console.log(`\nUsage: node index.js 需要轉換的字串`);
-    process.exit()
-} else {
-    origin = args[2];
+var pangu = require("./pangu");
+var OpenCC = require('opencc');
+
+function getArgText() {
+    var args = process.argv;
+    if (args.length < 3) {
+        console.log(`\nUsage: node index.js 需要轉換的字串`);
+        process.exit()
+    } else {
+        return args[2];
+    }
 }
 
-exports.origin = origin;
+function convertText(origin, conf='s2twp.json') {
+    // TODO: add other config
+    // Load the default Simplified to Traditional (Taiwan Standard) config
+    var openccInst = new OpenCC(conf);
+    // Sync API
+    var converted = openccInst.convertSync(origin);
+    converted = pangu.spacing(converted);
+    return converted;
+}
+
+exports.getArgText = getArgText;
+exports.convertText = convertText;
