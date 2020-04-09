82b56bbcf5c3a94765951187625a4c1ef9eaa258
JackKuo <kuohfjack@gmail.com>
2020-04-10 00:07:48 +0800

    Move conversion code to lib

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 'use strict'
-var pangu = require("./src/pangu");
-var OpenCC = require('opencc');
+
 var shudu = require("./src/shudu.js");
 
-// Load the default Simplified to Traditional config
-var opencc = new OpenCC('s2twp.json');
-
-// Sync API
-var origin = shudu.origin;
-var converted = opencc.convertSync(origin);
-converted = pangu.spacing(converted);
+// get text from stdin
+var origin = shudu.getArgText();
+var converted = shudu.convertText(origin);
 
 console.log(`\n原始: ${origin}\n轉換: ${converted}`)
