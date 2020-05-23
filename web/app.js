5b0d1910bb683ea94bf5de10e1e64097da15eef3
JackKuo <kuohfjack@gmail.com>
2020-05-23 18:45:10 +0800

    Add cors

--- web/app.js
+++ web/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 // const cookieParser = require('cookie-parser');
 // const logger = require('morgan');
 
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser());
 app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 
