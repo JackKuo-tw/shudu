40dd83547f59015706fb5087f3a8c23017c59d67
JackKuo <kuohfjack@gmail.com>
2020-08-10 00:21:00 +0800

    feat: add loading & error hint

--- web/public/javascripts/query.js
+++ web/public/javascripts/query.js
@@ -1,13 +1,20 @@
 $(function(){
     $('#convert button[type="submit"]').click(function(e){						   	 
         e.preventDefault();
+        tinyMCE.activeEditor.setProgressState(true)
         var origin = tinyMCE.activeEditor.getContent({format : 'raw'})
         console.log(origin);
         var punctuation = $('#punctuation').val();
         var data = {'origin': origin, 'punctuation': punctuation};
         $.post("/json", data, function(data){
             greet(data);
-        });
+        })
+            .always(()=>{
+                tinyMCE.activeEditor.setProgressState(false)
+            })
+            .fail(function() {
+                alert( "轉換失敗" );
+            });
     });
 
     var greet = function(msg){
@@ -18,5 +25,5 @@ $(function(){
         branding: false,
         language: "zh_TW",
         language_url : '/public/javascripts/zh_TW.js'
-   });
+    });
 });		 
