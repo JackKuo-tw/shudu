2550070a09da49864001bdb77a958ce936652d82
Jack Kuo <jackkuo@jackkuo.org>
2026-01-06 19:41:36 +0800

    enhance: upgrade to Chrome Manifest v3

--- browser_ext/background.js
+++ browser_ext/background.js
@@ -3,7 +3,7 @@ console.log(chrome.i18n.getMessage("background_running"))
 const browser = chrome
 const serverURL = "https://shudu.jackkuo.org/json";
 
-browser.browserAction.onClicked.addListener(handler);
+browser.action.onClicked.addListener(handler);
 browser.runtime.onMessage.addListener(sendText);
 
 function handler(tab) {
@@ -14,16 +14,16 @@ function sendText(msg) {
     const server = msg.server || serverURL;
 
     console.log('received data:', msg);
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         const tab = tabs[0];
 
         fetch(server, {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(msg.payload),
-            }).then(res => {
-                return res.json();
-            })
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(msg.payload),
+        }).then(res => {
+            return res.json();
+        })
             .then(resp => {
                 console.log('resp:', resp);
                 browser.tabs.sendMessage(tab.id, { status: 'success', resp });
@@ -39,7 +39,7 @@ function sendText(msg) {
 const version = 1.3;
 chrome.storage.sync.get({
     whats_new: 'v0',
-}, function(item) {
+}, function (item) {
     if (item.whats_new !== version) {
         chrome.tabs.create({ url: "release_notes.html" });
         chrome.storage.sync.set({ whats_new: version });
